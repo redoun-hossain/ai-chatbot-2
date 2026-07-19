3fbd6fd30529aaf7586658f169cf86403b229315
redoun-hossain <bd.myandme@gmail.com>
2026-07-20 02:51:58 +0600

    2.1

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 const CONFIG = {
-    WEBHOOK_TEST_URL: 'https://arfinsami178.app.n8n.cloud/webhook-test/2be4a2d3-0f20-4a72-947d-98c04335b2ed',
-    WEBHOOK_PROD_URL: 'https://arfinsami178.app.n8n.cloud/webhook/2be4a2d3-0f20-4a72-947d-98c04335b2ed',
+    WEBHOOK_TEST_URL: 'https://arfinsami178.app.n8n.cloud/webhook-test/34ddc073-f07e-4ef6-9c64-a41e2613569c',
+    WEBHOOK_PROD_URL: 'https://arfinsami178.app.n8n.cloud/webhook/34ddc073-f07e-4ef6-9c64-a41e2613569c',
     USE_TEST: false,
-    VERSION: 'Version-3.1',
+    VERSION: 'Version-2.1',
     get WEBHOOK_URL() {
         return this.USE_TEST ? this.WEBHOOK_TEST_URL : this.WEBHOOK_PROD_URL;
     }
