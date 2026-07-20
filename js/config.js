76bca995dfe0d2334d0ecbc22f0a1f1c5c2cba01
redoun-hossain <bd.myandme@gmail.com>
2026-07-20 18:02:10 +0600

    2.4

--- js/config.js
+++ js/config.js
@@ -2,7 +2,7 @@ const CONFIG = {
     WEBHOOK_TEST_URL: 'https://n8n.srv1106977.hstgr.cloud/webhook-test/34ddc073-f07e-4ef6-9c64-a41e2613569c',
     WEBHOOK_PROD_URL: 'https://n8n.srv1106977.hstgr.cloud/webhook/34ddc073-f07e-4ef6-9c64-a41e2613569c',
     USE_TEST: true,
-    VERSION: 'Version-2.3',
+    VERSION: 'Version-2.4',
     get WEBHOOK_URL() {
         return this.USE_TEST ? this.WEBHOOK_TEST_URL : this.WEBHOOK_PROD_URL;
     }
